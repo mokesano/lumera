c230198d7be493a721b46fac2b193254e55814fd
Rochmady <kampo.mokesano@gmail.com>
2026-05-01 03:18:45 +0700

    Update files dir

--- cache/t_compile/sangiapub^%%AF^AF0^AF094A69%%pdfViewer.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2026-04-04 05:48:14
-         compiled from file:/home/sangiaor/public_html/journals/plugins/generic/pdfJsViewer/templates//pdfViewer.tpl */ ?>
-
-
-<div id="pdfCanvasContainer" class="galley_view">
-	<iframe src="<?php echo $this->_tpl_vars['pdfUrl']; ?>
-" width="100%" height="100%" style="min-height: 500px;" allowfullscreen webkitallowfullscreen></iframe> 
-</div>
