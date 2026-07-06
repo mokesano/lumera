ae4c638c5d9ab6a48db233ffc0ba7a8e319297c4
Rochmady <srochmady@gmail.com>
2026-07-06 07:45:21 +0700

    Add Classical theme plugin implementation and update Gemfile.lock dependencies

--- plugins/themes/classical/index.php
+++ plugins/themes/classical/index.php
@@ -2,21 +2,21 @@
 declare(strict_types=1);
 
 /**
- * @defgroup plugins_themes_mpg
+ * @defgroup plugins_themes_classical Classical Theme
  */
 
 /**
- * @file plugins/themes/mpg/index.php
+ * @file plugins/themes/classical/index.php
  *
- * Copyright (c) 2013-2015 Simon Fraser University Library
- * Copyright (c) 2003-2015 John Willinsky
+ * Copyright (c) 2017-2026 Codecanau, LLC and CodeLumera, LLC
+ * Copyright (c) 2017-2026 Rochmady and Teams
  * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
  *
- * @ingroup plugins_themes_sangia
- * @brief Wrapper for "sangia classical" theme plugin.
+ * @ingroup plugins_themes_classical
+ * @brief Wrapper for "classical" theme plugin.
  *
  */
 
-require_once('SangiaClas.inc.php');
+require_once('Classical.inc.php');
 
-return new SangiaClas();
+return new Classical();
