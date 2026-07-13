ef9e8abdce04a2a61712335bec4a1b9bc51bcd88
Rochmady <srochmady@gmail.com>
2026-07-13 09:49:28 +0700

    Refactor ArticleEmailLogDAO for PHP 8.1+ strict compliance

--- classes/article/log/ArticleEmailLogDAO.inc.php
+++ classes/article/log/ArticleEmailLogDAO.inc.php
@@ -13,8 +13,6 @@ declare(strict_types=1);
  * @see EmailLogDAO
  *
  * @brief Extension to EmailLogDAO for article-specific log entries.
- *
- * [WIZDAM EDITION] Refactored for PHP 8.1+ Strict Compliance
  */
 
 import('lib.pkp.classes.log.EmailLogDAO');
