ff0a537af69b7af27b9032eedf78c3182fd0523f
Rochmady <srochmady@gmail.com>
2026-07-13 09:52:36 +0700

    Remove outdated comment for PHP 8.1+ strict compliance in ArticleEventLogDAO

--- classes/article/log/ArticleEventLogDAO.inc.php
+++ classes/article/log/ArticleEventLogDAO.inc.php
@@ -13,8 +13,6 @@ declare(strict_types=1);
  * @see EventLogDAO
  *
  * @brief Extension to EventLogDAO for article-specific log entries.
- *
- * [WIZDAM EDITION] Refactored for PHP 8.1+ Strict Compliance
  */
 
 import('lib.pkp.classes.log.EventLogDAO');
