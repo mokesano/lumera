19e6c384f6943aa89fac12cb0610272c5afc7bf4
Rochmady <srochmady@gmail.com>
2026-07-21 22:20:31 +0700

    Fix file path in documentation comment for ImageProcessor class

--- lib/wizdam/image/ImageProcessor.inc.php
+++ lib/wizdam/image/ImageProcessor.inc.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 /**
- * @file classes/image/ImageProcessor.inc.php
+ * @file lib/wizdam/image/ImageProcessor.inc.php
  *
  * Copyright (c) 2017-2026 Sangia Publishing House
  * Copyright (c) 2017-2026 Rochmady
