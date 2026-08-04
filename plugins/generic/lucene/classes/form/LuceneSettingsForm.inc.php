29bae64ec239e162e4b36f17de908ac95e09a422
Rochmady <srochmady@gmail.com>
2026-08-04 16:12:59 +0700

    refactor: Improve code readability and update constructor documentation in LuceneSettingsForm

--- plugins/generic/lucene/classes/form/LuceneSettingsForm.inc.php
+++ plugins/generic/lucene/classes/form/LuceneSettingsForm.inc.php
@@ -12,15 +12,11 @@ declare(strict_types=1);
  * @ingroup plugins_generic_lucene_classes_form
  *
  * @brief Form to configure Lucene/Solr search.
- *
- * @edition Wizdam Edition (PHP 8.x Compatible)
  */
 
 import('lib.pkp.classes.form.Form');
 import('lib.pkp.classes.form.validation.FormValidatorBoolean');
 
-// These are the first few letters of an md5 of '##placeholder##'.
-// FIXME: Any better idea how to prevent a password clash?
 define('LUCENE_PLUGIN_PASSWORD_PLACEHOLDER', '##5ca39841ab##');
 
 class LuceneSettingsForm extends Form {
@@ -29,45 +25,43 @@ class LuceneSettingsForm extends Form {
     protected $_plugin;
 
     /**
-     * Constructor
+     * Constructor.
+     * @param LucenePlugin $plugin
      */
     public function __construct($plugin) {
         $this->_plugin = $plugin;
         parent::__construct($plugin->getTemplatePath() . 'settingsForm.tpl');
 
-        // Server configuration.
         $this->addCheck(new FormValidatorUrl($this, 'searchEndpoint', FORM_VALIDATOR_REQUIRED_VALUE, 'plugins.generic.lucene.settings.searchEndpointRequired'));
-        // The username is used in HTTP basic authentication and according to RFC2617 it therefore may not contain a colon.
         $this->addCheck(new FormValidatorRegExp($this, 'username', FORM_VALIDATOR_REQUIRED_VALUE, 'plugins.generic.lucene.settings.usernameRequired', '/^[^:]+$/'));
         $this->addCheck(new FormValidator($this, 'password', FORM_VALIDATOR_REQUIRED_VALUE, 'plugins.generic.lucene.settings.passwordRequired'));
         $this->addCheck(new FormValidator($this, 'instId', FORM_VALIDATOR_REQUIRED_VALUE, 'plugins.generic.lucene.settings.instIdRequired'));
-
-        // Search feature configuration.
         $this->addCheck(new FormValidatorInSet($this, 'autosuggestType', FORM_VALIDATOR_REQUIRED_VALUE, 'plugins.generic.lucene.settings.internalError', array_keys($this->_getAutosuggestTypes())));
         
         $binaryFeatureSwitches = $this->_getFormFields(true);
-        foreach($binaryFeatureSwitches as $binaryFeatureSwitch) {
+        foreach ($binaryFeatureSwitches as $binaryFeatureSwitch) {
             $this->addCheck(new FormValidatorBoolean($this, $binaryFeatureSwitch, 'plugins.generic.lucene.settings.internalError'));
         }
     }
 
     /**
-     * [SHIM] Backward Compatibility
+     * [SHIM] Backward Compatibility.
+     * @param LucenePlugin $plugin
      */
     public function LuceneSettingsForm($plugin) {
         if (Config::getVar('debug', 'deprecation_warnings')) {
-            trigger_error("Class '" . get_class($this) . "' uses deprecated constructor parent::LuceneSettingsForm(). Please refactor to parent::__construct().", E_USER_DEPRECATED);
+            trigger_error(
+                "Class '" . get_class($this) . "' uses deprecated constructor " . get_class($this) . "(). Please refactor to use __construct().",
+                E_USER_DEPRECATED
+            );
         }
         $args = func_get_args();
-        call_user_func_array(array($this, '__construct'), $args);
+        call_user_func_array([$this, '__construct'], $args);
     }
 
-
-    //
-    // Implement template methods from Form.
-    //
     /**
-     * Initialize form data from the plugin settings.
+     * Initialize form data.
+     * @return void
      * @see Form::initData()
      */
     public function initData() {
@@ -75,59 +69,57 @@ class LuceneSettingsForm extends Form {
         foreach ($this->_getFormFields() as $fieldName) {
             $this->setData($fieldName, $plugin->getSetting(0, $fieldName));
         }
-        // We do not echo back the real password.
         $this->setData('password', LUCENE_PLUGIN_PASSWORD_PLACEHOLDER);
     }
 
     /**
      * Read user input data.
+     * @return void
      * @see Form::readInputData()
      */
     public function readInputData() {
         $this->readUserVars($this->_getFormFields());
         $request = PKPApplication::getRequest();
-        $password = $request->getUserVar('password');
+        $password = (string) $request->getUserVar('password');
         
         if ($password === LUCENE_PLUGIN_PASSWORD_PLACEHOLDER) {
-            $plugin = $this->_plugin;
-            $password = $plugin->getSetting(0, 'password');
+            $password = (string) $this->_plugin->getSetting(0, 'password');
         }
         $this->setData('password', $password);
     }
 
     /**
-     * Fetch the form template and display it.
-     * @see Form::fetch()
+     * Fetch the form template.
      * @param PKPRequest $request
-     * @param string $template
-     * @param boolean $display
+     * @param string|null $template
+     * @param bool $display
+     * @return string
+     * @see Form::fetch()
      */
     public function fetch($request, $template = null, $display = false) {
         $templateMgr = TemplateManager::getManager($request);
         $templateMgr->assign('autosuggestTypes', $this->_getAutosuggestTypes());
-        parent::fetch($request, $template, $display);
+        return parent::fetch($request, $template, $display);
     }
 
     /**
-     * Execute the form: persist the plugin settings.
+     * Execute the form.
+     * @param mixed $object
+     * @return void
      * @see Form::execute()
      */
     public function execute($object = null) {
         $plugin = $this->_plugin;
         $formFields = $this->_getFormFields();
         $formFields[] = 'password';
-        foreach($formFields as $formField) {
+        foreach ($formFields as $formField) {
             $plugin->updateSetting(0, $formField, $this->getData($formField), 'string');
         }
     }
 
-
-    //
-    // Private helper methods
-    //
     /**
-     * Return the field names of this form.
-     * @param boolean $booleanOnly Return only binary switches.
+     * Get form field names.
+     * @param bool $booleanOnly
      * @return array
      */
     protected function _getFormFields($booleanOnly = false) {
@@ -144,15 +136,11 @@ class LuceneSettingsForm extends Form {
             'autosuggestType'
         ];
         
-        if ($booleanOnly) {
-            return $booleanFormFields;
-        } else {
-            return array_merge($booleanFormFields, $otherFormFields);
-        }
+        return $booleanOnly ? $booleanFormFields : array_merge($booleanFormFields, $otherFormFields);
     }
 
     /**
-     * Return a list of auto-suggest types.
+     * Get auto-suggest types.
      * @return array
      */
     protected function _getAutosuggestTypes() {
@@ -161,5 +149,6 @@ class LuceneSettingsForm extends Form {
             SOLR_AUTOSUGGEST_FACETING => __('plugins.generic.lucene.settings.autosuggestTypeFaceting')
         ];
     }
+    
 }
 ?>
