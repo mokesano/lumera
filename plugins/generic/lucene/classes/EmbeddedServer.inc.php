d4eecb7c5ff84b9a7a133f6e08f27076c3c41ccc
Rochmady <srochmady@gmail.com>
2026-08-04 16:21:34 +0700

    refactor: Enhance constructor documentation and improve method readability in EmbeddedServer class

--- plugins/generic/lucene/classes/EmbeddedServer.inc.php
+++ plugins/generic/lucene/classes/EmbeddedServer.inc.php
@@ -12,137 +12,117 @@ declare(strict_types=1);
  * @ingroup plugins_generic_lucene_classes
  *
  * @brief Implements a PHP interface to administer the embedded solr server.
- *
- * @edition Wizdam Edition (PHP 8.x Compatible)
  */
 
 class EmbeddedServer {
 
     /**
-     * Constructor
+     * Constructor.
      */
     public function __construct() {
     }
 
     /**
-     * [SHIM] Backward Compatibility
+     * [SHIM] Backward Compatibility.
      */
     public function EmbeddedServer() {
         if (Config::getVar('debug', 'deprecation_warnings')) {
-            trigger_error("Class '" . get_class($this) . "' uses deprecated constructor parent::EmbeddedServer(). Please refactor to parent::__construct().", E_USER_DEPRECATED);
+            trigger_error(
+                "Class '" . get_class($this) . "' uses deprecated constructor " . get_class($this) . "(). Please refactor to use __construct().",
+                E_USER_DEPRECATED
+            );
         }
         $args = func_get_args();
-        call_user_func_array(array($this, '__construct'), $args);
+        call_user_func_array([$this, '__construct'], $args);
     }
 
     //
     // Public API
     //
+
     /**
      * Start the embedded server.
-     *
-     * NB: The web service can take quite a bit longer than the
-     * process to start. So if you want to be sure you should
-     * instantiate SolrWebService and wait until it's status is
-     * SOLR_STATUS_ONLINE.
-     *
-     * @return boolean true if the server started, otherwise false.
+     * @return bool
      */
     public function start() {
-        // Run the start command.
         return $this->_runScript('start.sh');
     }
 
     /**
      * Stop the embedded server.
-     *
-     * @return boolean true if the server stopped, otherwise false.
+     * @return bool
      */
     public function stop() {
-        // Run the stop command.
         return $this->_runScript('stop.sh');
     }
 
     /**
-     * Stop the embedded server and wait until it actually exited.
-     *
-     * @return boolean true if the server stopped, otherwise false.
+     * Stop the embedded server and wait until it exits.
+     * @return bool
      */
     public function stopAndWait() {
-        $running = $this->isRunning();
-        if ($running) {
-            // Stop the server.
-            $success = $this->stop();
-            if (!$success) return false;
-
-            // Give the server time to actually go down.
-            while($this->isRunning()) sleep(1);
+        if ($this->isRunning()) {
+            if (!$this->stop()) {
+                return false;
+            }
+            while ($this->isRunning()) {
+                sleep(1);
+            }
         }
         return true;
     }
 
     /**
      * Check whether the embedded server is currently running.
-     *
-     * @return boolean true, if the server is running, otherwise false.
+     * @return bool
      */
     public function isRunning() {
-        $returnValue = $this->_runScript('check.sh');
-        return ($returnValue === true);
+        return $this->_runScript('check.sh');
     }
-
 
     //
     // Private helper methods
     //
+
     /**
-     * Find the script directory.
-     *
+     * Get the script directory path.
      * @return string
      */
     protected function _getScriptDirectory() {
-        // Modernized path resolution
         return dirname(__DIR__) . '/embedded/bin/';
     }
 
     /**
-     * Run the given script.
-     *
-     * @param string $command The script to be executed.
-     *
-     * @return boolean true if the command executed successfully, otherwise false.
+     * Execute a shell script.
+     * @param string $command
+     * @return bool
      */
     protected function _runScript($command) {
-        // Get the log file name.
-        $logFile = Config::getVar('files', 'files_dir') . '/lucene/solr-php.log';
-
-        // [SECURITY FIX] Ensure the script directory exists before changing context
+        $logFile = (string) Config::getVar('files', 'files_dir') . '/lucene/solr-php.log';
         $scriptDirectory = $this->_getScriptDirectory();
+        
         if (!is_dir($scriptDirectory)) {
             error_log("Lucene Plugin Error: Script directory not found at " . $scriptDirectory);
             return false;
         }
 
-        // [SECURITY FIX] Use escapeshellarg for the log path to prevent injection if path has spaces/special chars
-        // Original: $command = $scriptDirectory . $command . ' 2>&1 >>"' . $logFile . '" </dev/null';
-        $fullCommand = './' . $command . ' 2>&1 >>' . escapeshellarg($logFile) . ' </dev/null';
-
-        // Execute the command.
+        $fullCommand = './' . escapeshellcmd($command) . ' 2>&1 >>' . escapeshellarg($logFile) . ' </dev/null';
         $workingDirectory = getcwd();
-        
-        // Change dir, execute, and revert
+        if ($workingDirectory === false) {
+            return false;
+        }
+
         if (chdir($scriptDirectory)) {
-            // $dummy is initialized to avoid notice in PHP 8 if exec fills it
-            $dummy = []; 
-            exec($fullCommand, $dummy, $returnStatus);
+            $output = []; 
+            $returnStatus = -1;
+            exec($fullCommand, $output, $returnStatus);
             chdir($workingDirectory);
             
-            // Return the result.
             return ($returnStatus === 0);
         }
 
         return false;
     }
+    
 }
-
 ?>
