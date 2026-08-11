43f28b63b5be220d81acb358fee6952d6d436717
Rochmady <srochmady@gmail.com>
2026-08-12 00:42:56 +0700

    refactor(ArticleGalley): improve deprecated constructor warning message and update method call

--- classes/article/ArticleGalley.inc.php
+++ classes/article/ArticleGalley.inc.php
@@ -27,16 +27,17 @@ class ArticleGalley extends ArticleFile {
     }
 
     /**
-     * [SHIM] Backward Compatibility
+     * [SHIM] Backward Compatibility.
      */
     public function ArticleGalley() {
         if (Config::getVar('debug', 'deprecation_warnings')) {
             trigger_error(
-                "Class '" . get_class($this) . "' uses deprecated constructor parent::ArticleGalley(). Please refactor to parent::__construct().", 
+                "Class '" . get_class($this) . "' uses deprecated constructor " . get_class($this) . "(). Please refactor to use __construct().",
                 E_USER_DEPRECATED
             );
         }
-        self::__construct();
+        $args = func_get_args();
+        call_user_func_array([$this, '__construct'], $args);
     }
 
     /**
