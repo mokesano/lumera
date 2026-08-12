fcb9669eee0f41fb59b921b60d5f3dcb42daf048
Rochmady <srochmady@gmail.com>
2026-08-13 03:44:50 +0700

    fix(WizdamArticleStyle): enhance DOI link validation with URL parsing for allowed hosts

--- plugins/themes/sangiapub/js/WizdamArticleStyle.js
+++ plugins/themes/sangiapub/js/WizdamArticleStyle.js
@@ -2399,10 +2399,21 @@ $(document).ready(function() {
                 });
             }
 
+            const isDoiHostUrl = (rawUrl) => {
+                try {
+                    const normalizedUrl = rawUrl.startsWith('www.') ? `https://${rawUrl}` : rawUrl;
+                    const parsedUrl = new URL(normalizedUrl);
+                    const allowedDoiHosts = ['doi.org', 'dx.doi.org'];
+                    return allowedDoiHosts.includes(parsedUrl.hostname.toLowerCase());
+                } catch (e) {
+                    return false;
+                }
+            };
+
             const httpMatches = content.match(/https?:\/\/[^\s]+|www\.[^\s]+/g);
             if (httpMatches) {
                 httpMatches.forEach(httpUrl => {
-                    if (!httpUrl.includes('doi.org')) addLinkWithLoading(referenceLinks, httpUrl, 'View Source', '<svg focusable="false" viewBox="0 0 8 8" height="20" aria-label="Opens in new window" class="icon icon-arrow-up-right-tiny arrow-external-link"><path d="M1.12949 2.1072V1H7V6.85795H5.89111V2.90281L0.784057 8L0 7.21635L5.11902 2.1072H1.12949Z"></path></svg>');
+                    if (!isDoiHostUrl(httpUrl)) addLinkWithLoading(referenceLinks, httpUrl, 'View Source', '<svg focusable="false" viewBox="0 0 8 8" height="20" aria-label="Opens in new window" class="icon icon-arrow-up-right-tiny arrow-external-link"><path d="M1.12949 2.1072V1H7V6.85795H5.89111V2.90281L0.784057 8L0 7.21635L5.11902 2.1072H1.12949Z"></path></svg>');
                 });
             }
 
