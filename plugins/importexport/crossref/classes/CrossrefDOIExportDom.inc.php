9ddd7e11abd82177c18007df42f4f4d75d82d844
Rochmady <srochmady@gmail.com>
2026-08-25 05:30:47 +0700

    Fix file reference in CrossrefDOIExportDom documentation

--- plugins/importexport/crossref/classes/CrossrefDOIExportDom.inc.php
+++ plugins/importexport/crossref/classes/CrossrefDOIExportDom.inc.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 /**
- * @file plugins/importexport/crossref/classes/DOIExportDom.inc.php
+ * @file plugins/importexport/crossref/classes/CrossrefDOIExportDom.inc.php
  *
  * Copyright (c) 2013-2019 Simon Fraser University
  * Copyright (c) 2003-2019 John Willinsky
