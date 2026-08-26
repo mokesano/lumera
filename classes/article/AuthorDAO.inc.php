96d5bb9d641808e1110cdfbed6690eb4fc577151
Rochmady <srochmady@gmail.com>
2026-08-27 05:33:50 +0700

    Improve deprecation warning message in AuthorDAO constructor for clarity

--- classes/article/AuthorDAO.inc.php
+++ classes/article/AuthorDAO.inc.php
@@ -33,9 +33,13 @@ class AuthorDAO extends PKPAuthorDAO {
      */
     public function AuthorDAO() {
         if (Config::getVar('debug', 'deprecation_warnings')) {
-            trigger_error('Class AuthorDAO uses deprecated constructor. Please refactor to __construct().', E_USER_DEPRECATED);
+            trigger_error(
+                "Class '" . get_class($this) . "' uses deprecated constructor " . get_class($this) . "(). Please refactor to use __construct().",
+                E_USER_DEPRECATED
+            );
         }
-        self::__construct();
+        $args = func_get_args();
+        call_user_func_array([$this, '__construct'], $args);
     }
 
     /**
